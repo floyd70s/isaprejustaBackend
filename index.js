883b00c6e1e36155ae78ebbf53eb8d6053b609e6
casa <casa@iMac-de-casa-6.local>
2020-05-27 21:26:38 -0400

    ip localhost

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var port = process.env.PORT || 3977;
 
 //mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://35.153.98.79:27017/isaprejusta', (err, res) => {
+mongoose.connect('mongodb://localhost:27017/isaprejusta', (err, res) => {
     if (err) {
         throw err;
     }
     else {
         console.log('la base esta corriendo correctamente...');
         app.listen(port, function () {
-            console.log("v0.2 - el servidor del api rest activo en http://35.153.98.79:" + port)
+            console.log("v0.2 - el servidor del api rest activo en http://localhost:" + port)
 
             let now = new Date();
             console.log('La fecha actual es', now);
