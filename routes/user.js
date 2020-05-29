fda4e1bd36e770c87b790b90a02b76bf8bf2877e
casa <casa@iMac-de-casa-6.local>
2020-05-28 22:24:19 -0400

    path upload

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,11 @@ var api= express.Router();
 var md_auth= require('../middlewares/authenticated')
 
 var multipart=require('connect-multiparty')
-var md_upload=multipart({uploadDir:'./uploads/users'})
+var md_upload=multipart({uploadDir:'uploads/users'})
+
+// var multer = require('multer')({
+// dest: 'public/uploads' })
+
 
 api.get('/probando-controlador',UserController.pruebas)
 api.post('/saveUser',UserController.saveUser)
